fdb9627bda6b98e69bf1c4c7e09cf43dd16e0ef8
Sougata Bandyopadhyay <mail2sou@gmail.com>
2021-09-21 21:31:01 +0530

    Added new test

--- cypress/integration/TC02.filterFunctionality.spec.js
+++ cypress/integration/TC02.filterFunctionality.spec.js
@@ -16,6 +16,30 @@ describe('Filter and Clear Filter Functionality', () => {
         cy.findByText(eventPageData.totalSubmissionCount).should('be.visible')
     })
 
+    it('Filter using a organizations', () => {
+        let filterCount
+        cy.findByText('Add filter').click()
+        cy.findByText('Keywords').click()
+        cy.findByText('Organizations').click()
+        cy.findAllByPlaceholderText('Search for organizations').type(eventPageData.organizationFilter)
+        cy.wait(5000)
+        cy.get('div.MuiCollapse-container.MuiCollapse-entered > div > div > div > div > div.MuiButtonBase-root.MuiListItem-root.MuiListItem-gutters.MuiListItem-button > div').click()
+        cy.wait(5000)
+        cy.get('div.MuiCollapse-container.MuiCollapse-entered > div > div > div > div > div.MuiButtonBase-root.MuiListItem-root.MuiListItem-gutters.MuiListItem-button > div').parent().children('span.sc-gsDJrp.fbXlhZ').then(($filter) => {
+            filterCount = $filter.text()
+            filterCount = filterCount.match(/\d+/)[0]
+            cy.log(filterCount)
+        })
+        cy.findByText('Close').click()
+        cy.wait(5000)
+        cy.get('div:nth-child(3) > p').then(($submission) => {
+            let submissionCount = $submission.text()
+            submissionCount = submissionCount.replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '').match(/\d+/)[0]
+            cy.log(submissionCount)
+            expect(submissionCount).to.eq(filterCount)
+        })
+    })
+
     it('Filter using a keyword', () => {
         let filterCount
         cy.findByText('Add filter').click()
